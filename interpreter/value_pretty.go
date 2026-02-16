40d3b6f92c9bf58b80e77996e19684767a1e7443
Nicolas Modrzyk <hellonico@gmail.com>
2026-02-16 14:32:50 +0100

    color formatting for cells output

--- interpreter/value_pretty.go
+++ interpreter/value_pretty.go
@@ -2,13 +2,41 @@ package interpreter
 
 import (
 	"bytes"
+	"fmt"
 	"sort"
 	"strings"
+)
+
+// ANSI Color Codes
+const (
+	ColorReset   = "\033[0m"
+	ColorKey     = "\033[1;34m" // Bold Blue
+	ColorString  = "\033[32m"   // Green
+	ColorNumber  = "\033[33m"   // Yellow
+	ColorBool    = "\033[35m"   // Purple
+	ColorNull    = "\033[1;30m" // Bold Grey
+	ColorSymbol  = "\033[37m"   // White (for brackets, commas)
 )
 
 // PrettyPrinter is an interface for values that can be pretty-printed
 type PrettyPrinter interface {
 	Pretty(indent int) string
+}
+
+func colorize(val Value) string {
+	s := val.Inspect()
+	switch val.Type() {
+	case STRING:
+		return ColorString + s + ColorReset
+	case INTEGER, FLOAT:
+		return ColorNumber + s + ColorReset
+	case BOOLEAN:
+		return ColorBool + s + ColorReset
+	case NULL:
+		return ColorNull + s + ColorReset
+	default:
+		return s
+	}
 }
 
 func (a *Array) Pretty(indent int) string {
@@ -26,7 +54,7 @@ func (a *Array) Pretty(indent int) string {
 		if pp, ok := el.(PrettyPrinter); ok {
 			out.WriteString(pp.Pretty(indent + 1))
 		} else {
-			out.WriteString(el.Inspect())
+			out.WriteString(colorize(el))
 		}
 		
 		if i < len(a.Elements)-1 {
@@ -61,13 +89,13 @@ func (o *Object) Pretty(indent int) string {
 	for i, k := range keys {
 		v := o.Pairs[k]
 		out.WriteString(indentStr)
-		out.WriteString(k)
+		out.WriteString(ColorKey + fmt.Sprintf("%q", k) + ColorReset)
 		out.WriteString(": ")
 		
 		if pp, ok := v.(PrettyPrinter); ok {
 			out.WriteString(pp.Pretty(indent + 1))
 		} else {
-			out.WriteString(v.Inspect())
+			out.WriteString(colorize(v))
 		}
 		
 		if i < len(keys)-1 {
@@ -100,7 +128,19 @@ func (m *Map) Pretty(indent int) string {
 	}
 	pairs := make([]kv, 0, len(m.Pairs))
 	for k, v := range m.Pairs {
-		pairs = append(pairs, kv{k, v, formatMapKey(k)})
+		keyStr := formatMapKey(k)
+		// Colorize key based on type if possible, or just treat as key color
+		// For maps, keys can be anything. Let's color them as keys for consistency or values?
+		// Let's stick to key color for consistency with Objects
+		if k.Type == STRING {
+			keyStr = ColorKey + keyStr + ColorReset
+		} else {
+			// For non-string keys, maybe use their value color?
+			// But they are keys. Let's use Key color to distinguish.
+			keyStr = ColorKey + keyStr + ColorReset
+		}
+		
+		pairs = append(pairs, kv{k, v, keyStr})
 	}
 	sort.Slice(pairs, func(i, j int) bool {
 		return pairs[i].s < pairs[j].s
@@ -114,7 +154,7 @@ func (m *Map) Pretty(indent int) string {
 		if pp, ok := pair.v.(PrettyPrinter); ok {
 			out.WriteString(pp.Pretty(indent + 1))
 		} else {
-			out.WriteString(pair.v.Inspect())
+			out.WriteString(colorize(pair.v))
 		}
 		
 		if i < len(pairs)-1 {
@@ -147,7 +187,7 @@ func (s *Set) Pretty(indent int) string {
 	
 	for i, k := range keys {
 		out.WriteString(indentStr)
-		out.WriteString(k)
+		out.WriteString(ColorString + k + ColorReset) // Treat set elements as values? Strings mostly?
 		
 		if i < len(keys)-1 {
 			out.WriteString(",")
